325821adcd0dad2e3f698ce3724d950d4f67dfb9
Sabyasachi Rakshit <sabyasachirakshitacs19@gmail.com>
2022-07-11 20:01:32 +0530

    date functionality added

--- logic.js
+++ logic.js
@@ -3,6 +3,8 @@ console.log("JS linked!");
 function showNotes() {
   let notes_title = [];
   let notes_desc = [];
+  let notes_date = [];
+  let notesDate = localStorage.getItem("notesDate");
   let notesTitle = localStorage.getItem("notesTitle");
   let notesDescription = localStorage.getItem("notesDescription");
   if (notesTitle) {
@@ -10,6 +12,9 @@ function showNotes() {
   }
   if (notesDescription) {
     notes_desc = JSON.parse(notesDescription);
+  }
+  if (notesDate) {
+    notes_date = JSON.parse(notesDate);
   }
   if (notes_title.length == 0 && notes_desc.length == 0) {
     document.getElementById("notes-content").innerHTML =
@@ -23,6 +28,7 @@ function showNotes() {
                             <p class="card-text">${notes_desc[i]}</p>
           <button class="btn btn-primary mx-3" onClick=editNote(${i})>Edit Note</button>
           <button class="btn btn-danger" onClick=deleteNote(${i})>Delete Note</button>
+          <p>Added on ${notes_date[i]}</p>
         </div>
       </div>`;
     }
@@ -81,54 +87,83 @@ function resetContent() {
 function modifyNote(index) {
   let notes_title = [];
   let notes_desc = [];
+  let notes_date = [];
+  let notesDate = localStorage.getItem("notesDate");
   let notesTitle = localStorage.getItem("notesTitle");
   let notesDescription = localStorage.getItem("notesDescription");
+
   if (notesTitle) {
     notes_title = JSON.parse(notesTitle);
   }
   if (notesDescription) {
     notes_desc = JSON.parse(notesDescription);
   }
+  if (notesDate) {
+    notes_date = JSON.parse(notesDate);
+  }
+
   notes_title[index] = document.getElementById("titleArea").value;
   notes_desc[index] = document.getElementById("descriptionArea").value;
+  let current_date = new Date();
+  notes_date[index] = current_date.toDateString();
   localStorage.setItem("notesTitle", JSON.stringify(notes_title));
   localStorage.setItem("notesDescription", JSON.stringify(notes_desc));
+  localStorage.setItem("notesDate", JSON.stringify(notes_date));
   resetContent();
 }
 
 function deleteNote(index) {
   let notes_title = [];
   let notes_desc = [];
+  let notes_date = [];
+  let notesDate = localStorage.getItem("notesDate");
   let notesTitle = localStorage.getItem("notesTitle");
   let notesDescription = localStorage.getItem("notesDescription");
+
   if (notesTitle) {
     notes_title = JSON.parse(notesTitle);
   }
   if (notesDescription) {
     notes_desc = JSON.parse(notesDescription);
   }
+  if (notesDate) {
+    notes_date = JSON.parse(notesDate);
+  }
   notes_title.splice(index, 1);
   notes_desc.splice(index, 1);
+  notes_date.splice(index, 1);
   localStorage.setItem("notesTitle", JSON.stringify(notes_title));
   localStorage.setItem("notesDescription", JSON.stringify(notes_desc));
+  localStorage.setItem("notesDate", JSON.stringify(notes_date));
   showNotes();
 }
 
 function addNote(title, desc) {
   let notes_title = [];
   let notes_desc = [];
+  let notes_date = [];
+  let notesDate = localStorage.getItem("notesDate");
   let notesTitle = localStorage.getItem("notesTitle");
   let notesDescription = localStorage.getItem("notesDescription");
+
   if (notesTitle) {
     notes_title = JSON.parse(notesTitle);
   }
   if (notesDescription) {
     notes_desc = JSON.parse(notesDescription);
   }
+  if (notesDate) {
+    notes_date = JSON.parse(notesDate);
+  }
   notes_title.push(title);
   notes_desc.push(desc);
+  let current_date = new Date();
+  notes_date.push(current_date.toDateString());
   localStorage.setItem("notesTitle", JSON.stringify(notes_title));
   localStorage.setItem("notesDescription", JSON.stringify(notes_desc));
+  localStorage.setItem("notesDate", JSON.stringify(notes_date));
+  document.getElementById("titleArea").value = "";
+  document.getElementById("descriptionArea").value = "";
   showNotes();
 }
 showNotes();
